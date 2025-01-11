e1d940396ba8fd7e9f7e509ca0bdc4037219d31a
ahmet seyhan <ahmet.seyhan@bilgeadamboost.com>
2025-01-11 15:04:06 +0300

    Task-42 tamamlandı. Business Entity eklendi. İçi doldurulmadı.

---
+++ HRBoost.Entity/Business.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRBoost.Entity
+{
+    public class Business:BaseEntity
+    {
+        public Subscription Subscription { get; set; }
+    }
+}
