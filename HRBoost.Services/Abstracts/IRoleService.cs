f12367f7aed24caaf2f2383d59f1210c4fd164fe
Öykü Aleyna Görür ( BilgeAdam Boost ) <oykualeyna.gorur@bilgeadamboost.com>
2025-01-11 17:23:24 +0300

    Task 37-38-39-40

---
+++ HRBoost.Services/Abstracts/IRoleService.cs
@@ -0,0 +1,22 @@
+﻿using HRBoost.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRBoost.Services.Abstracts
+{
+    internal interface IRoleService
+    {
+        Task<bool> Add(string name);
+        Task<List<Role>> GetRoles(Expression<Func<Role,bool>>exp);
+        Task<List<Role>> GetAll();
+        Task<Role>FindByName(string name);
+        Task<Role> FindById(string id);
+        Task<bool>DeleteRole(string id);
+        Task<bool> Update(Role role);
+
+    }
+}
