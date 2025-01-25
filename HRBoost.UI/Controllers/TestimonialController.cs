fcaf17255961c9638adb3ae47bc57c56ab7630d8
ahmet seyhan <ahmet.seyhan@bilgeadamboost.com>
2025-01-25 19:12:51 +0300

    Task-87 tamamlandı. Bug var, çözülecek.

---
+++ HRBoost.UI/Controllers/TestimonialController.cs
@@ -0,0 +1,56 @@
+﻿using HRBoost.Services.Abstracts;
+using HRBoost.UI.Models.VM;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRBoost.UI.Controllers
+{
+    public class TestimonialController : Controller
+{
+    private readonly IBusinessService _businessService;
+    private readonly IUserService _userService;
+
+    public TestimonialController(IUserService userService, IBusinessService businessService)
+    {
+        _userService = userService;
+        _businessService = businessService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        YorumVM selectedYorum = null; 
+        var ul = await _userService.GetAllUsersAsync();
+        var bs = await _businessService.GetBy(x => x.BusinessComment != null);
+        foreach (var b in bs)
+        {
+            foreach (var user in ul)
+            {
+                if (await _userService.GetUserRole(user) == "BusinessManager")
+                {
+                    if (user.BusinessId == b.Id)
+                    {
+                        YorumVM yorum = new YorumVM
+                        {
+                            BusinessName = b.BusinessName,
+                            BusinessComment = b.BusinessComment,
+                            BusinessLogo = b.BusinessLogo,
+                            UserPhoto = user.UserPhoto,
+                            FirstName = user.FirstName,
+                            LastName = user.LastName,
+                            Position = user.Position
+                        };
+
+                        selectedYorum = yorum; 
+                        break;
+                    }
+                }
+            }
+            if (selectedYorum != null)
+            {
+                break; 
+            }
+        }
+
+        return View(new List<YorumVM> { selectedYorum }); 
+    }
+}
+}
