e1819b3fe426f2937b5307597347b9652996b374
ahmet seyhan <ahmet.seyhan@bilgeadamboost.com>
2025-02-01 22:22:05 +0300

    Task-123 tamamlandı.

--- HRBoost.Entity/Debit.cs
+++ HRBoost.Entity/Debit.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,7 @@ namespace HRBoost.Entity
     {
         public string Name { get; set; }
         public int Amount { get; set; }
-        public Guid PersonelId { get; set; }
+        public Guid UserId { get; set; }
         public string? Description { get; set; }
         public Guid BusinessId { get; set; }
 
