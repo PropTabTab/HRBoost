7cb023f78ee29f81ce8a65e801db72343c9c94c4
ahmet seyhan <ahmet.seyhan@bilgeadamboost.com>
2025-02-05 00:37:18 +0300

    Bazı cshtml düzenlemeleri ve mail gönderme işlemleri tamamlandı.

---
+++ HRBoost.UI/Controllers/ContactController.cs
@@ -0,0 +1,23 @@
+﻿using HRBoost.Services.Abstracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRBoost.UI.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IEmailService _emailService;
+        public ContactController(IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Mail(string name, string email, string subject, string text)
+        {
+            await _emailService.SendEmail(email, "Mesajınız İletilmiştir..." , $"Sayın {name}, \nMesajınız iletilmiştir.");
+            await _emailService.SendEmail("hrboost2@gmail.com", $"{email} Tarafından: {subject}", text);
+
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
