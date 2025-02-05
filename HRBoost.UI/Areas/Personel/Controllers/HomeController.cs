c3732d9a39a497eeec2e066619a855045f12e1a6
Mustafa Dara Özevin <mustafadara.ozevin@outlook.com>
2025-02-05 20:59:56 +0300

    kalan izinler

--- HRBoost.UI/Areas/Personel/Controllers/HomeController.cs
+++ HRBoost.UI/Areas/Personel/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using HRBoost.Services.Abstracts;
+using HRBoost.UI.Areas.Personel.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,19 +14,27 @@ namespace HRBoost.UI.Areas.Personel.Controllers
         private IBusinessService _businessService;
         private readonly IEmailService _emailService;
         private readonly IHolidayService _holidayService;
+        private readonly IPermissionRequestService _permissionService;
 
-        public HomeController(IUserService userService, IBusinessService businessService, IEmailService emailService, IHolidayService holidayService)
+        public HomeController(IUserService userService, IBusinessService businessService, IEmailService emailService, IHolidayService holidayService, IPermissionRequestService permissionService)
         {
             _userService = userService;
             _businessService = businessService;
             _emailService = emailService;
             _holidayService = holidayService;
+            _permissionService = permissionService;
         }
 
         public async Task<IActionResult> Index()
         {
-            var holidays = await _holidayService.getHolidays();
-            return View(holidays);
+            PersonelVM vm = new PersonelVM();
+            var user = await _userService.GetUserByMail(User.Identity.Name);
+            vm.holidays = await _holidayService.getHolidays();
+            var permissions = (await _permissionService.GetBy(x=> x.UserId==user.Id && x.Status== Shared.Enums.Status.Approved));
+            foreach (var permission in permissions) {
+                vm.permissionDuration += (permission.EndDate - permission.StartDate).Days;
+            }
+            return View(vm);
         }
 
     }
